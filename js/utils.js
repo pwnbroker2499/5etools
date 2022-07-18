a289934e805137ce889b3a68feedb5c2dc1f5a43
TheGiddyLimit <TheGiddyLimit@users.noreply.github.com>
2022-07-18 21:33:53 +0000

    Deploying to gh-pages from @ 5etools-mirror-1/5etools-mirror-1.github.io@6636901c4d79c957295aa72df4d1f0b390ac0b73 🚀

--- js/utils.js
+++ js/utils.js
@@ -6,8 +6,8 @@ IS_NODE = typeof module !== "undefined";
 if (IS_NODE) require("./parser.js");
 
 // in deployment, `IS_DEPLOYED = "<version number>";` should be set below.
-IS_DEPLOYED="v1.162.0";
-VERSION_NUMBER = /* 5ETOOLS_VERSION__OPEN */"1.162.0"/* 5ETOOLS_VERSION__CLOSE */;
+IS_DEPLOYED="v1.163.0";
+VERSION_NUMBER = /* 5ETOOLS_VERSION__OPEN */"1.163.0"/* 5ETOOLS_VERSION__CLOSE */;
 DEPLOYED_STATIC_ROOT = ""; // "https://static.5etools.com/"; // FIXME re-enable this when we have a CDN again
 // for the roll20 script to set
 IS_VTT = false;
